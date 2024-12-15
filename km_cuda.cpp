a7157fbcc9365b1bdd925967a85ceaf1b72a81dc
dramaticjamster <43726367+dramaticjamster@users.noreply.github.com>
2024-12-15 14:29:24 -0600

    Update km_cuda.cpp

--- km_cuda.cpp
+++ km_cuda.cpp
@@ -157,7 +157,7 @@ int iter = 0;
 
 
 int _grid_size_n = 9;
-int _grid_size[_grid_size_n] = {4096, 2048, 1024, 512, 256, 128, 64, 32, 16};
+int _grid_size[_grid_size_n] = {2048, 1024, 512, 256, 128, 64, 32, 16};
 int _block_size_n = 6;
 int _block_size[_block_size_n] = {2048, 1024, 512, 256, 128, 64};
 double cur_best_time = std::numeric_limits<double>::max();
