efd5471a0384c66078bbb7b093fecbc7ef280c04
dramaticjamster <43726367+dramaticjamster@users.noreply.github.com>
2024-12-15 16:03:16 -0600

    Update km_cuda.cpp

--- km_cuda.cpp
+++ km_cuda.cpp
@@ -165,8 +165,10 @@ int cur_best[] = {-1, -1};
 
 for (int b_idx = 0; b_idx < _block_size_n; b_idx++){
   for (int g_idx = 0; g_idx < _grid_size_n; g_idx++){
-    int g = _grid_size[g_idx];
-    int b = _block_size[b_idx];
+    //int g = _grid_size[g_idx];
+    //int b = _block_size[b_idx];
+    int g = 8192;
+    int b = 1024;
 
     km_main(g, b, points, classes, clusters, cluster_indexes, new_cluster_indexes,
         num_pts, num_cl, dim, MAX_ITER, time_ptr);
